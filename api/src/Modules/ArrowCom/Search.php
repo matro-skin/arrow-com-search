b84acfbe336bd8c21003f75c7394c43bcb80ed0d
psprokofiev <ps.prokofiev@gmail.com>
2020-04-05 10:06:18 +0400

    added header

--- api/src/Modules/ArrowCom/Search.php
+++ api/src/Modules/ArrowCom/Search.php
@@ -51,6 +51,7 @@ class Search implements ShouldRespond
 		$request->setHeaders([
 			'Accept'        => 'application/json',
 			'Authorization' => sprintf("Bearer %s", $this->token->access_token),
+			'User-Agent'    => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
 		])->setQuery($this->getQuery());
 
 		\PartsSearch\Search::log('Requesting...');
